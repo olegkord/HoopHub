473f2723d7fe07bbffe9bf8c7ecb498f8f62569a
Oleg <oleg.kord@gmail.com>
2015-11-21 17:06:27 -0500

    added create user route

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,7 +13,14 @@ router.route('/new')
   .post( (req,res) => {
     console.log('Creating a new user');
 
-    let newUserParams = req.body.params;
+    let newUserParams = req.body;
+
+    let newUser = new User(newUserParams)
+    newUser.save( (error) => {
+      if (error) res.status(400).send({message: error.errmsg});
+
+      else res.status(201).send({message: "successfully created user"});
+    })
   })
 
 
@@ -25,7 +32,7 @@ router.route('/:id')
     //Body parser?
     // let userID = params.body.id;
 
-    let userID = req.params.id;
+    let userID = req.body.id;
 
     User.find( {id: userID}, (error, user) => {
       if (error) throw error;
@@ -37,24 +44,24 @@ router.route('/:id')
   .put( (req, res) => {
     //route to edit user information
     console.log('Editing User information');
-    let userParams = req.params;
+    let userParams = req.body;
 
     User.findByIdAndUpdate(userParams.id,
       {$set: userParams},
       (error, user) => {
-        if(error) res.status(401).send({message: error.errmsg});
+        if(error) res.status(400).send({message: error.errmsg});
 
-        else return res.status(200).send({message: "User update succesful"});
+        else return res.status(202).send({message: "User update succesful"});
       })
   })
 
   .delete( (req,res) => {
     //route to delete a user
     console.log('Deleting a user');
-    let userID = req.params.id;
+    let userID = req.body.id;
 
     User.findByIdAndRemove(userID, (error) => {
-      if(error) res.status(401).send({message: error.errmsg});
+      if(error) res.status(400).send({message: error.errmsg});
 
       else return res.status(200).send({message: "User delete successful"})
     });
