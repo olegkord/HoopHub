c9caaa117917897ad5e4d1e8735393361219e732
Steve <steve@Steves-MacBook-Pro.local>
2015-11-21 16:26:14 -0500

    added reset function

--- public/script.js
+++ public/script.js
@@ -1,1 +1,48 @@
 'use strict';
+
+$(function(){
+
+  let renderTemplate_show_user = Handlebars.compile($('template#user-template').html());
+  let renderTemplate_create_user = Handlebars.compile($('template#new-user-template').html());
+  let renderTemplate_edit_user = Handlebars.compile($('template#edit-user-template').html());
+
+  let renderTemplate_show_player_profile = Handlebars.compile($('template#player-profile-template').html());
+  let renderTemplate_show_player_stats = Handlebars.compile($('template#player-stats-template').html());
+  let renderTemplate_show_player_updates = Handlebars.compile($('template#player-update-template').html());
+
+  // =================================================================
+  // Link Click Events ===============================================
+  // =================================================================
+
+    $('#create_user_button')((e) => {
+      
+
+
+    })
+
+
+
+
+  // =================================================================
+  // Render templates ================================================
+  // =================================================================
+
+
+
+
+
+
+
+  let resetUserView = () => {
+    $('.user-profile-div').empty();
+    $('.user-player-list-div').empty();
+  }
+
+  let resetPlayerView = () => {
+    $('.player-profile-div').empty();
+    $('.player-update-div').empty();
+    $('.player-twtr-div').empty();
+    $('.player-stats-div').empty();
+  }
+
+});
