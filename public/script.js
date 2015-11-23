5763161b827d381a66d7eb5ddf59a12370cd9e41
Adnan Rokadia <Adnan@Adnans-MacBook-Pro.local>
2015-11-23 15:32:34 -0500

    updated player list template

--- public/script.js
+++ public/script.js
@@ -147,7 +147,7 @@ $(function(){
     resetPlayerList();
 
     let $list = ('.user_player_list');
-    let compiledTemplate = renderTemplate_show_user_player_list(data);
+    let compiledTemplate = renderTemplate_show_user_player_list({user: data});
     $list.html('').append(compiledTemplate);
   }
 
