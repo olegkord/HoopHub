eb774770d4ad4a53f3d1d9569a575d1200387c52
Adnan Rokadia <Adnan@Adnans-MacBook-Pro.local>
2015-11-29 16:57:27 -0500

    changed multer pictures route

--- controllers/pictures_controller.js
+++ controllers/pictures_controller.js
@@ -2,14 +2,21 @@ let express = require('express');
 let router = express.Router();
 let multer = require('multer')
 
-let uploading = multer({
-  dest: __dirname + '../public/uploads/',
-  limits: {fileSize: 1000000, files:1},
-})
+// let uploading = multer({
+//   dest: __dirname + './public/uploads/',
+//   limits: {fileSize: 1000000, files:1},
+// })
 
-router.route('/upload') uploading, function(req, res) {
-  console.log('in multer!');
+app.post('/',[ multer({ dest: './public/uploads/'}), function(req, res){
+    console.log(req.body) // form fields
+    console.log(req.files) // form files
+    res.status(204).end()
+}]);
 
-})
+app.listen(3000);
+// router.route('/upload') uploading, function(req, res) {
+//   console.log('in multer!');
+//
+// })
 
 module.exports = router
