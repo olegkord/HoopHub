5723fc9537c8c5ed7b31ed27afb142d7a656e5ce
Steve <steve.l.chen@gmail.com>
2015-12-03 18:44:35 -0500

    revert twtr controller back

--- controllers/twitter_controller.js
+++ controllers/twitter_controller.js
@@ -21,7 +21,7 @@ var T = new Twit({
   , access_token_secret:  ACCESS_TOKEN_SECRET  // Your Access Token Secret
 });
 
-let connectionString = process.env.MONGOLAB_URI || (process.env.MONGO_URL + '/HoopHub');
+let connectionString = process.env.MONGOLAB_URI ? process.env.MONGO_URL + '/HoopHub' : 'HoopHub';
 let collectionName = 'NBA';
 let hashtag = '#NBA';
 
