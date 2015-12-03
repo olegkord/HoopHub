8aa9908e1ae801f873b6a197329d3f7d76671353
Steve <steve.l.chen@gmail.com>
2015-12-03 16:53:47 -0500

    tweets going into HoopHub db

--- controllers/twitter_controller.js
+++ controllers/twitter_controller.js
@@ -21,7 +21,7 @@ var T = new Twit({
   , access_token_secret:  ACCESS_TOKEN_SECRET  // Your Access Token Secret
 });
 
-let connectionString = process.env.MONGO_URL ? process.env.MONGO_URL + '/tweets' : 'tweets';
+let connectionString = process.env.MONGOLAB_URI || process.env.MONGO_URL + '/HoopHub' : 'HoopHub';
 let collectionName = 'NBA';
 let hashtag = '#NBA';
 
